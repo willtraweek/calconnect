b0dcdeaa8b7a4f27bd94ed72bab8dc863d5a18d2
Will Traweek <will.traweek@gmail.com>
2020-03-31 14:37:48 -0500

    set debug to false

--- application.py
+++ application.py
@@ -13,5 +13,5 @@ def index():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    app.debug = True
+    app.debug = False
     app.run()
