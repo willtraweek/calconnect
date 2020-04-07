4f828e8fb48655bd83cc87ba2320921a947c6358
mitchmorrison2 <mitchm@smu.edu>
2020-04-07 08:38:46 -0700

    updating all files

--- credentials.py
+++ credentials.py
@@ -6,5 +6,5 @@ scopes = [
     "https://www.googleapis.com/auth/calendar",
     "https://www.googleapis.com/auth/admin.directory.resource.calendar.readonly",
 ]
-store = file.Storage("credentials.json")
+store = file.Storage("../cred/credentials.json")
 creds = store.get()
