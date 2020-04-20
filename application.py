056fd442d796db454f5c6173aa8b31eb09091c01
Will Traweek <will.traweek@gmail.com>
2020-04-20 14:44:58 -0500

    stage SSL signing

--- application.py
+++ application.py
@@ -75,4 +75,4 @@ def index():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    app.run()
+    app.run(ssl_context=('calconnectcsr.pem','calconnect.pem'))
