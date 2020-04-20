870c9d2872509926e394174d9bb0d0adc9118226
Will Traweek <will.traweek@gmail.com>
2020-04-20 14:43:43 -0500

    fix running in Debug mode

--- application.py
+++ application.py
@@ -75,5 +75,4 @@ def index():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    app.debug = True
     app.run()
